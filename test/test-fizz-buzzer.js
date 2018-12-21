b8ab27db246c1e069a0f3c4e4570975c0a5575a7
Publikwerker <jason.michael.hoffman@gmail.com>
2018-12-21 09:08:03 -0800

    update with my version
    same but different

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,50 +1,45 @@
 'use strict';
 
-const should = require('chai').should();
-const {expect} = require('chai');
+// import chai, declare expect variable
+const expect = require('chai').expect;
 
+// import fizzBuzzer
 const fizzBuzzer = require('../fizzBuzzer');
 
-describe('testing the fizzBuzzer function', function() {
-  const normalCases = [
-    {num: 15, expected: 'fizz-buzz'},
-    {num: 5, expected: 'buzz'},
-    {num: 3, expected: 'fizz'}
-  ];
+// unit tests for our `fizzBuzzer` function
+describe('testing fizzBuzzer app', function() {
 
-  it('15 should print fizz-buzz', function() {
-    const answer = fizzBuzzer(normalCases[0].num);
-    answer.should.equal(normalCases[0].expected);
-  });
+    // test normal case
+    it('should return appropriate response', function () {
+        // range of normal inputs
+        const normalCases = [
+            {num: 15, expected: 'fizz-buzz'},
+            {num: 5, expected: 'buzz'},
+            {num: 3, expected: 'fizz'}
+        ];
+        // for each input, 'fizzBuzzer' should
+        // produce the expected value
+        normalCases.forEach(function(input) {
+            const answer = fizzBuzzer(input.num);
+            expect(answer).to.equal(input.expected);
+        });
 
-  it('5 should print buzz', function() {
-    const answer = fizzBuzzer(normalCases[1].num);
-    answer.should.equal(normalCases[1].expected);
-  });
-  
-  it('3 should print fizz', function() {
-    const answer = fizzBuzzer(normalCases[2].num);
-    answer.should.equal(normalCases[2].expected);
-  });
-
-  it('should complete the fizz-buzz sequence', function() {
-    normalCases.forEach(input => {
-      const answer = fizzBuzzer(input.num);
-      answer.should.equal(input.expected);
     });
-  });
 
-  it('should raise error if inputs are not numbers', function() {
-    const badInputs = [
-      'bad',
-      undefined,
-      'really bad'
-    ];
-
-    badInputs.forEach(function(input) {
-      expect(function() {
-        fizzBuzzer(input).to.throw(Error);
-      });
+    // test exceptional cases
+    it('should raise error if arg is not a number', function() {
+        // range of bad inputs that are not numbers
+        const badInputs = [
+            'a',
+            '1',
+            false,
+        ];
+        // prove that an error is raised for bad inputs
+        badInputs.forEach(function(input) {
+            expect(function() {
+                fizzBuzzer(input).to.throw(Error);
+            });
+        });
     });
-  });
 });
+
